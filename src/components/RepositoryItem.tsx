2fb1fc9a1fcdb64f54d5f81baf3ccfd87a89d39b
Alan <alanoliveira123bit@gmail.com>
2022-05-14 16:59:28 -0300

    style adjust

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -7,13 +7,17 @@ interface propsPropriety {
 export function RepositoryItem(props: any) {
     return (
         <li className="m-6">
-            <strong className="text-2xl font-bold">{props.repository.name}</strong>
+            <div className="flex flex-row justify-between">
+                <strong className="text-2xl font-bold pr-20">{props.repository.name}</strong>
+                <p className="text-gray-300 text-sm mt-1 hover:text-gray-500 transition-all">{props.repository.language}</p>
+            </div>
             <p className="text-lg text-gray-500 my-1">{props.repository.description}</p>
 
-            <a href={props.repository.html_url} className="text-gray-400 hover:text-slate-300 transition-all font-light">
+            <a href={props.repository.html_url} className="text-gray-500 hover:text-slate-300 transition-all font-light">
                 access here
             </a>
 
+            <p className=" text-gray-300 text-sm mt-1 ">{props.repository.full_name}</p>
 
             <div className="py-4">
                 <div className="w-full border-t border-gray-300"></div>
